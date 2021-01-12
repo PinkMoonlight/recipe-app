58a59b4cf09ad8f4ab76c3ea9bef25aac523f9dc
Pink Moonlight <kate.cherie25@gmail.com>
2021-01-12 14:46:09 -0800

    refactored code into components

---
+++ src/Recipe-theme.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let Theme = () => {
+  return (
+    <div>
+      {/* <h3>Desserts</h3> */}
+      <h3 className="baby-shower-theme">
+        Kate and Modassar's Virtual Baby Shower
+      </h3>
+    </div>
+  );
+};
+
+export default Theme;
